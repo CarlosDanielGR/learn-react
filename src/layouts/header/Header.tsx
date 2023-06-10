32ba6bcb2c8aa3c1908161e8278b27c01f95d60d
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-06-10 18:58:09 -0500

    styles: first styles to header

--- src/layouts/header/Header.tsx
+++ src/layouts/header/Header.tsx
@@ -1,7 +1,12 @@
+import { Logo } from "./components";
 import "./header.scss";
 
 function Header() {
-  return <div className="header">Header</div>;
+  return (
+    <div className="header">
+      <Logo />
+    </div>
+  );
 }
 
 export default Header;
