a6fce68e5de7daed9b40daea0c715f40361fb046
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-06-11 13:13:57 -0500

    feat: configure link active

--- src/layouts/sidebar/Sidebar.tsx
+++ src/layouts/sidebar/Sidebar.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import { Accordion, Link } from "./components";
 import { LINKS_SIDEBAR } from "./data/sidebar.data";
 import "./styles/Sidebar.scss";
 
 function Sidebar() {
+  const [linkActive, setLinkActive] = useState(0);
+
+  const handleLinkActive = (id: number) => {
+    setLinkActive(id);
+  };
+
   return (
     <section className="sidebar">
       {LINKS_SIDEBAR.map((link) => {
         if (link.children) return <Accordion />;
-        return <Link key={link.id} link={link} />;
+        return (
+          <Link
+            key={link.id}
+            link={link}
+            onActive={handleLinkActive}
+            isActive={linkActive === link.id}
+          />
+        );
       })}
     </section>
   );
--- src/layouts/sidebar/components/Link.tsx
+++ src/layouts/sidebar/components/Link.tsx
@@ -3,11 +3,20 @@ import "./../styles/Link.scss";
 
 interface LinkProps {
   link: ISidebar;
+  isActive: boolean;
+  onActive: (id: number) => void;
 }
 
-function Link({ link }: LinkProps) {
+function Link({ link, onActive, isActive }: LinkProps) {
+  const handleActive = () => {
+    onActive(link.id);
+  };
+
   return (
-    <a className="link" href={link.route}>
+    <a
+      className={"link" + " " + (isActive ? "link--active" : "")}
+      onClick={handleActive}
+    >
       {link.name}
     </a>
   );
