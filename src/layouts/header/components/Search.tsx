375b0047f0211482d257c9de2795af27f71a227a
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-06-10 20:34:41 -0500

    feat: search input partially completed

--- src/layouts/header/components/Search.tsx
+++ src/layouts/header/components/Search.tsx
@@ -1,10 +1,30 @@
+import { ChangeEvent, KeyboardEvent, useState } from "react";
 import { HiSearch } from "react-icons/hi";
+import "./../styles/Search.scss";
 
 function Search() {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearchChange = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter")
+      console.log("Search init with value: ", searchValue);
+  };
+
+  const onSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
-    <div>
-      <input type="text" placeholder="Search the docs" />
-      <HiSearch />
+    <div className="search">
+      <input
+        type="text"
+        className="search__input"
+        placeholder="Search the docs"
+        value={searchValue}
+        onChange={onSearchChange}
+        onKeyUp={handleSearchChange}
+      />
+      <HiSearch className="search__icon" />
     </div>
   );
 }
