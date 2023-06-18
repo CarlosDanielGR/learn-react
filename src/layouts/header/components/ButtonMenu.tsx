7ce553bb9fef47b6a36765e822cd003f4b19ba07
CarlosDanielGR <carlos.dgr.12@gmail.com>
2023-06-17 20:26:56 -0500

    styles: change button menu component

--- src/layouts/header/components/ButtonMenu.tsx
+++ src/layouts/header/components/ButtonMenu.tsx
@@ -1,27 +1,12 @@
-import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { Button } from "@mui/material";
 import "./../styles/ButtonModal.scss";
-import { Menu } from ".";
 
 function ButtonMenu() {
-  const [isOpenModal, setOpenModal] = useState(false);
-
-  const toggleButtonMenu = (isOpen: boolean) => {
-    console.log("Open modal", !isOpen);
-    setOpenModal(!isOpen);
-  };
-
   return (
-    <div className="buttonModal">
-      <button
-        type="button"
-        className="buttonModal__toggle"
-        onClick={() => toggleButtonMenu(isOpenModal)}
-      >
-        <GiHamburgerMenu />
-        <Menu isOpen={isOpenModal} />
-      </button>
-    </div>
+    <Button className="buttonMenu" aria-label="Menu">
+      <GiHamburgerMenu />
+    </Button>
   );
 }
 
